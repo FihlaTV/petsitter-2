e12a2f251fbd6ae7cf9c53a740ec5f208a6a2b75
IonelU <johnyurlich@gmail.com>
2018-05-26 11:31:39 +0200

    aggiunta pg index e profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,21 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { ProfilePage  } from '../pages/profile/profile';
+import { IndexPage } from '../pages/index';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    ProfilePage,
+    IndexPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +29,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    ProfilePage,
+    IndexPage
   ],
   providers: [
     StatusBar,
